17475b5c1e9785314bb779dd47fd1c47c85c41fe
Jordi Batlle <joenbb@gmail.com>
2023-01-17 21:35:36 +0100

    second commit

--- Enterprise-Java-Development/src/Main.java
+++ Enterprise-Java-Development/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        int[] array1 = {1, 2, 3, 4, 5};
+        differenceOfNumbers(array1);
+
+    }
+
+    public static void differenceOfNumbers(int[] array){
+        int largest = array[0];
+        int smallest = array[0];
+        for (int i=0; i<array.length; i++){
+            if (largest<array[i]){
+                largest = array[i];
+            } else if (smallest>array[i]){
+                smallest = array[i];
+
+            }
+        }
+        System.out.println(largest-smallest);
     }
 }
