81c2ed13275a1e4532901beca1478ab066a5df69
Jordi Batlle <joenbb@gmail.com>
2023-01-18 20:11:35 +0100

    3commit

--- Enterprise-Java-Development/src/Main.java
+++ Enterprise-Java-Development/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         int[] array1 = {1, 2, 3, 4, 5};
         differenceOfNumbers(array1);
         findTheSmallest(array1);
+        mathematicalExpresion(5, 3);
     }
 
     public static void differenceOfNumbers(int[] array){
@@ -22,11 +23,18 @@ public class Main {
 
     public static void findTheSmallest(int[] array){
         int smallest = array[0];
+        int secondSmallest = 0;
         for (int i=0; i<array.length; i++){
             if (smallest>array[i]){
                 smallest = array[i];
+            }if (smallest<secondSmallest && secondSmallest<array[i]){
+                secondSmallest = array[i];
             }
         }
-        System.out.println(smallest);
+        System.out.println("smallest: " + smallest + " second smallest: " + secondSmallest);
+    }
+
+    public static void mathematicalExpresion(int x, int y){
+        System.out.println(Math.pow(x, 2)+Math.pow(Math.pow(4, y)/5-x,2));
     }
 }
